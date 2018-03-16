93b41c97a1f05ddf5dfca45c43004051c6628398
Joel Rorseth <joelrorseth@gmail.com>
2018-03-16 18:23:21 -0400

    Design 3D Earth model in SceneKit using open source images

--- World-Tour.playground/Contents.swift
+++ World-Tour.playground/Contents.swift
@@ -1,83 +1,164 @@
-//: A SpriteKit based Playground
+//
+// World Tour
+//
+// A fully interactive 3D scene, displaying Earth and animating paths and
+// an airplane to travel them.
+//
 
+import Cocoa
+import SceneKit
+import CoreLocation
+import GLKit
 import PlaygroundSupport
-import SpriteKit
 
-class GameScene: SKScene {
+
+class WorldScene: SCNScene  {
     
-    private var label : SKLabelNode!
-    private var spinnyNode : SKShapeNode!
+    // Scene nodes
+    let earthNode = SCNNode()
+    let cameraNode = SCNNode()
+    let sunNode = SCNNode()
+    let cloudsNode = SCNNode()
     
-    override func didMove(to view: SKView) {
-        // Get label node from scene and store it for use later
-        label = childNode(withName: "//helloLabel") as? SKLabelNode
-        label.alpha = 0.0
-        let fadeInOut = SKAction.sequence([.fadeIn(withDuration: 2.0),
-                                           .fadeOut(withDuration: 2.0)])
-        label.run(.repeatForever(fadeInOut))
+    let sunNodeRotationSpeed: CGFloat  = CGFloat(Double.pi/6)
+    let earthNodeRotationSpeed: CGFloat = CGFloat(Double.pi/40)
+    var earthNodeRotation: CGFloat = 0
+    var sunNodeRotation: CGFloat = CGFloat(Double.pi/2)
+    
+    
+    override init() {
+        super.init()
         
-        // Create shape node to use during mouse interaction
-        let w = (size.width + size.height) * 0.05
+        setupEarth()
+        setupCamera()
+        setupSun()
+        setupCloudsAndHalo()
         
-        spinnyNode = SKShapeNode(rectOf: CGSize(width: w, height: w), cornerRadius: w * 0.3)
-        spinnyNode.lineWidth = 2.5
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupEarth() {
         
-        let fadeAndRemove = SKAction.sequence([.wait(forDuration: 0.5),
-                                               .fadeOut(withDuration: 0.5),
-                                               .removeFromParent()])
-        spinnyNode.run(.repeatForever(.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-        spinnyNode.run(fadeAndRemove)
-    }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
+        let material = SCNMaterial()
+        material.ambient.contents = NSColor(white:  0.8, alpha: 1)
         
-        n.position = pos
-        n.strokeColor = SKColor.green
-        addChild(n)
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        guard let n = self.spinnyNode.copy() as? SKShapeNode else { return }
+        // Use the open source images
+        material.diffuse.contents = NSImage(named: NSImage.Name(rawValue: "diffuse"))
+        material.specular.contents = NSImage(named: NSImage.Name(rawValue: "specular"))
+        material.normal.contents = NSImage(named: NSImage.Name(rawValue: "normal"))
+        material.emission.contents = NSImage(named: NSImage.Name(rawValue: "lights"))
+
+        material.specular.intensity = 1
+        material.shininess = 0.04
+        material.multiply.contents = NSColor(white:  0.8, alpha: 1)
         
-        n.position = pos
-        n.strokeColor = SKColor.blue
-        addChild(n)
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        guard let n = spinnyNode.copy() as? SKShapeNode else { return }
+        let sphere = SCNSphere(radius: 5)
+        sphere.firstMaterial = material
+        earthNode.geometry = sphere
         
-        n.position = pos
-        n.strokeColor = SKColor.red
-        addChild(n)
+        // Add Earth node to scene object graph
+        self.rootNode.addChildNode(earthNode)
     }
     
-    override func mouseDown(with event: NSEvent) {
-        touchDown(atPoint: event.location(in: self))
+    func setupCamera() {
+        
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 12)
+        
+        let light = SCNLight()
+        light.type = SCNLight.LightType.ambient
+        light.color = NSColor(white: 0.01, alpha: 1.0)
+        cameraNode.light = light
+        
+        // Add camera node to scene object graph
+        rootNode.addChildNode(cameraNode)
+        
     }
     
-    override func mouseDragged(with event: NSEvent) {
-        touchMoved(toPoint: event.location(in: self))
+    func setupSun() {
+        
+        let light = SCNLight()
+        light.type = SCNLight.LightType.directional
+        sunNode.light = light
+        
+        // Sun is rotated out of view
+        sunNode.rotation = SCNVector4(x: 0.0, y: 1.0, z: 0.0, w: CGFloat(sunNodeRotation))
+        rootNode.addChildNode(sunNode)
     }
     
-    override func mouseUp(with event: NSEvent) {
-        touchUp(atPoint: event.location(in: self))
+    
+    func setupCloudsAndHalo() {
+        
+        //Set up clouds material radius slightly bigger than earth
+        let clouds = SCNSphere(radius: 5.075)
+        clouds.segmentCount = 120;
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = NSColor.white
+        material.transparent.contents = NSImage(named: NSImage.Name(rawValue: "clouds"))
+        material.transparencyMode = SCNTransparencyMode.rgbZero;
+        material.locksAmbientWithDiffuse = true
+        material.writesToDepthBuffer = false
+        
+        // Load GLSL code snippet for Halo effects
+        do {
+            if let path = Bundle.main.path(forResource: "halo", ofType: "glsl") {
+                let glsl = try NSString(contentsOf: URL(fileURLWithPath: path),
+                                                encoding: String.Encoding.utf8.rawValue)
+                material.shaderModifiers = [SCNShaderModifierEntryPoint.fragment : glsl as String]
+            }
+        } catch { print("Error: Could not encode glsl as NSString") }
+        
+        clouds.firstMaterial = material
+        cloudsNode.geometry = clouds
+        cloudsNode.opacity = 0.35
+        cloudsNode.rotation = SCNVector4Make(0, 1, 0, 0);
+        
+        earthNode.addChildNode(cloudsNode)
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    // Leftwards continuous revolving animation
+    func revolve(node: SCNNode, value: CGFloat, increase: CGFloat) -> CGFloat {
+        var rotation = value
+        
+        if value < CGFloat(-Double.pi * 2) {
+            
+            rotation = value + CGFloat(Double.pi*2)
+            node.rotation = SCNVector4(x: 0.0, y: 1.0, z: 0.0, w: rotation)
+        }
+        
+        return rotation - increase
     }
 }
 
-// Load the SKScene from 'GameScene.sks'
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
-if let scene = GameScene(fileNamed: "GameScene") {
-    // Set the scale mode to scale to fit the window
-    scene.scaleMode = .aspectFill
+
+class WorldView: SCNView {
     
-    // Present the scene
-    sceneView.presentScene(scene)
+     let worldScene = WorldScene()
+    
+    override init(frame: NSRect, options: [String : Any]? = nil) {
+        super.init(frame: frame, options: options)
+        
+        // Establish default view properties
+        allowsCameraControl = true
+        autoenablesDefaultLighting = true
+        
+        // TODO: View background should be space or something
+        backgroundColor = NSColor.black
+        
+        self.scene = worldScene
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
-PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
+
+// Instantiate iPad resolution proportionate view
+PlaygroundPage.current.liveView = WorldView(frame:
+    NSRect(x: 0, y: 0, width: 512, height: 384))
+
